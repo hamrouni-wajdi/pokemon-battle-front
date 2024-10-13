6d4a00b95d684b5d04ef1d3aed8a76061fab9282
hamrouni-wajdi <hamrouniwajdi14@gmail.com>
2024-10-13 14:45:56 +0100

    bootstrap

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule,NgbNavConfig  } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [NgbNavModule,NgbModule],
+  providers: [NgbNavConfig],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })

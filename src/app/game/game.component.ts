07c7046d34c41612e650aa1d65b2c855ccc06d85
hamrouni-wajdi <hamrouniwajdi14@gmail.com>
2024-10-13 04:27:53 +0100

    starting with factors

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,58 +2,92 @@ import { Component } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { Pokemon } from '../types/Pokemon';
 import { PokemonListComponent } from '../pokemon-list/pokemon-list.component';
-import { PokemonComponent } from "../pokemon/pokemon.component";
+import { PokemonComponent } from '../pokemon/pokemon.component';
 import { Team } from '../types/team';
 import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [PokemonListComponent, PokemonComponent,CommonModule],
+  imports: [PokemonListComponent, PokemonComponent, CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
 export class GameComponent {
-  firstTeam:Team = {name:"",pokemons:[]}
-  secondTeam:Team = {name:"",pokemons:[]}
-  firstTeamPokemon:Pokemon={name:'',image:'',life:0,id:0,power:0,type:1}
-  secondTeamPokemon:Pokemon={name:'',image:'',life:0,id:0,power:0,type:1}
+  firstTeam: Team = { name: '', pokemons: [] };
+  secondTeam: Team = { name: '', pokemons: [] };
+  round:number=1;
+  firstTeamPokemon_factor: any
+  secondTeamPokemon_factor:any
+  firstTeamPokemon: Pokemon = {
+    name: '',
+    image: '',
+    life: 0,
+    id: 0,
+    power: 0,
+    type: 1,
+  };
+  secondTeamPokemon: Pokemon = {
+    name: '',
+    image: '',
+    life: 0,
+    id: 0,
+    power: 0,
+    type: 1,
+  };
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {}
+  ngAfterViewInit() {
+    this.creategame('Ashely', 'My Team');
+  }
+
+  public async creategame(firstTeamName: string, secondTeamName: string) {
+    this.apiService
+      .getBattleTeam(firstTeamName, secondTeamName)
+      .subscribe((data) => {
+        console.log(data);
+        this.firstTeam = {
+          pokemons: data.teams.firstTeam,
+          name: data.firstTeamName,
+        };
+        this.secondTeam = {
+          pokemons: data.teams.secondTeam,
+          name: data.secondTeamName,
+        };
+        this.secondTeamPokemon = this.secondTeam.pokemons[0];
+        this.firstTeamPokemon = this.firstTeam.pokemons[0];
+      });
+  }
+
+  public selectPokemons(teamName: string, pokemonIndex: number) {
+    console.log(teamName, this.firstTeam.name);
+    if (this.firstTeam.name === teamName) {
+      this.firstTeamPokemon = this.firstTeam.pokemons[pokemonIndex];
+      return;
+    } else if (this.secondTeam.name === teamName) {
+      console.log(this.secondTeam.pokemons[pokemonIndex++]);
+      this.secondTeamPokemon = this.secondTeam.pokemons[pokemonIndex];
+      return;
+    } else {
+      console.log('cannot select pokemons');
+    }
+  }
   
-  constructor(private apiService :ApiService){}
+public async getFactor(type1:number, type2:number){
+  try{
+     return this.apiService.getFactor(type1,type2)
+  }
+  catch(error){
+    console.log(error)
+    return null
+  }
+}
 
-  ngOnInit(){
-  }
-  ngAfterViewInit(){
-    this.creategame("Ashely","My Team");
-    this.selectPokemons("Ashely",0)
-  }
+public async fight(){
+  await this.apiService.getFactor(this.firstTeamPokemon.type,this.secondTeamPokemon.type).subscribe((factor)=>this.firstTeamPokemon_factor = factor)
+  await this.apiService.getFactor(this.secondTeamPokemon.type,this.firstTeamPokemon.type).subscribe((factor)=>this.secondTeamPokemon_factor = factor)
+}
 
-  public async creategame(firstTeamName:string,secondTeamName:string){
-   this.apiService.getBattleTeam(firstTeamName, secondTeamName).subscribe((data)=>{
-        console.log(data)
-        this.firstTeam = {pokemons:data.teams.firstTeam,name:data.firstTeamName}
-        this.secondTeam = {pokemons:data.teams.secondTeam,name:data.secondTeamName}
-        this.secondTeamPokemon = this.secondTeam.pokemons[0]
-        this.firstTeamPokemon = this.firstTeam.pokemons[0]
-   
-    })
-  }
-
- public selectPokemons(teamName:string,pokemonIndex:number){
-  console.log(teamName,this.firstTeam.name)
-  if(this.firstTeam.name === teamName){
-    this.firstTeamPokemon = this.firstTeam.pokemons[pokemonIndex];
-    return;
-  }
-  else if(this.secondTeam.name === teamName) {
-    console.log(this.secondTeam.pokemons[pokemonIndex++])
-    this.secondTeamPokemon = this.secondTeam.pokemons[pokemonIndex];
-    return;
-  }
-  else{
-    console.log("cannot select pokemons")
-  }
-  }
-
- }
-
+}
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Pokemon } from '../types/Pokemon';
 
@@ -19,5 +19,11 @@ export class ApiService {
    
     public getBattleTeam(firstTeamName:string,secondTeamName:string):Observable<any>{
       return this.http.post(`${this.baseUrl}/team/getPokemonsInBattle`,{firstTeamName,secondTeamName})
+    }
+    public getFactor(type1:number,type2:number){
+      let params = new HttpParams()
+      .set('type1', type1)
+      .set('type2', type2);
+      return this.http.post(`${this.baseUrl}/game/factor`,{type1,type2})
     }
 }

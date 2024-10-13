a1bbbc909a6fc86baf790b20096a5b9a5000e763
hamrouni-wajdi <hamrouniwajdi14@gmail.com>
2024-10-14 00:12:46 +0100

    improved navbar and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,9 @@ import { TeamComponent } from './team/team.component';
 import { GameComponent } from './game/game.component';
 
 export const routes: Routes = [
-    { path: '', component: PokemonDashboardComponent },    
+    { path: 'pokemons', component: PokemonDashboardComponent },    
     { path: 'favorites', component: FavoritesComponent},
     { path: 'teams', component: TeamComponent},
     { path: 'battle', component: GameComponent},
+    { path: '', component: PokemonDashboardComponent },
 ];

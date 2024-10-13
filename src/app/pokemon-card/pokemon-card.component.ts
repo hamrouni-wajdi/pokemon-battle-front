607b17a9ea18b55c97fea2588896facd1964917d
hamrouni-wajdi <hamrouniwajdi14@gmail.com>
2024-10-13 18:50:41 +0100

    displayed power and inputs

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,17 +1,28 @@
 import { Component, Input } from '@angular/core';
 import { Pokemon } from '../types/Pokemon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pokemon-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pokemon-card.component.html',
   styleUrl: './pokemon-card.component.scss'
 })
 export class PokemonCardComponent {
-
+  displayForm:boolean=false;
+  editButtonText:string='Edit';
   @Input()
   pokemon!:Pokemon
+
+  public onEdit(){
+    this.displayForm=!this.displayForm
+    if(this.displayForm){
+      this.editButtonText = "Save";
+      return;
+    }
+    this.editButtonText = "Edit";
+   }  
 
   public parsetype(input: string | number): string | number | undefined {
     const types: { [key: number]: string } = {
--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,14 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { Pokemon } from '../types/Pokemon';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-pokemon',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pokemon.component.html',
   styleUrl: './pokemon.component.scss'
 })
 export class PokemonComponent {
 
+ displayForm:boolean=false;
  @Input()
  pokemon!:Pokemon;
+
+ public onEdit(){
+  this.displayForm=true
+ }
+
 }
